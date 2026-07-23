70a2868fc170c7db11f1b2a07f1804cd11e291bf
Divyansh <Krrish020805@gmail.com>
2026-07-23 19:47:17 +0530

    Time: 15 ms (23.59%), Space: 48.5 MB (5.09%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans=new int[2];
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]==nums[i-1]) ans[0]=nums[i];
+            if (nums[i]-nums[i-1]>1) ans[1]=nums[i-1]+1;
+        }
+        if (nums[0]!=1) ans[1]=1;
+        if (nums[nums.length-1]!=nums.length) ans[1]=nums.length;
+        return ans;
+    }
+}
