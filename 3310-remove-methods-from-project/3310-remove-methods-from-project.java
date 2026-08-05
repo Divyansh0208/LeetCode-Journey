7b332a74450e518777836bbf8fec1c72d5d5c702
Divyansh <Krrish020805@gmail.com>
2026-08-05 11:36:34 +0530

    Time: 60 ms (82.69%), Space: 293.7 MB (22.12%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer>[] list=new ArrayList[n];
+        for(int i=0;i<n;i++) list[i]=new ArrayList<>();
+
+        for(int[] arr:invocations) list[arr[0]].add(arr[1]);
+
+        boolean[] sus =new boolean[n];
+        dfs(k,list,sus);
+
+        for(int[] arr:invocations){
+            if(!sus[arr[0]] && sus[arr[1]]){
+                List<Integer> a=new ArrayList<>();
+                for(int i=0;i<n;i++) a.add(i);
+                return a;
+            }
+        }
+        List<Integer> a=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(!sus[i]) a.add(i);
+        }
+        return a;
+    }
+    private void dfs(int node, List<Integer>[] arr, boolean[] sus){
+        if(sus[node]) return;
+        sus[node]=true;
+
+        for(int f:arr[node]) dfs(f,arr,sus);
+    }
+}
