5a514abf86a07f66c0563a04d472f74e00b71a7f
Divyansh <Krrish020805@gmail.com>
2026-09-08 22:31:50 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,14 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        long t=0;
-        for(long th=1000;th<=n;th*=1000){
-            t+=(n-th+1);
-        }
-        return (int)t;
-    }
-    public static void main(String[] args){
-        Solution sol=new Solution();
-        System.out.println(sol.countCommas(1002));
-        System.out.println(sol.countCommas(998));
+        if(n<=999) return 0;
+        else return n-999;
     }
 }
