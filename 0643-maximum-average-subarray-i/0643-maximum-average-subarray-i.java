3d3fa4e6f3de391226408810c7793b062cac3479
Divyansh <Krrish020805@gmail.com>
2026-09-09 23:20:20 +0530

    Time: 3 ms (74.04%), Space: 69.5 MB (79.74%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<k;i++) sum += nums[i];
         int max = sum;
         for(int i=k;i<nums.length;i++){
-            sum = sum - nums[i-k] + nums[i];
+            sum += nums[i] - nums[i - k];
             max = Math.max(max,sum);
         }
         return (double)max/k;
